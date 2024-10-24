b80ac8d5d005c3a0d0695e1cbb5e3f2d3a938a79
sifter0926 <sifter0926@gmail.com>
2024-10-24 14:04:08 +0900

    댓글 backend 완료

--- src/main/java/org/pgm/japdemo/controller/ReplyController.java
+++ src/main/java/org/pgm/japdemo/controller/ReplyController.java
@@ -3,15 +3,14 @@ package org.pgm.japdemo.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.pgm.japdemo.dto.PageRequestDTO;
+import org.pgm.japdemo.dto.PageResponseDTO;
 import org.pgm.japdemo.dto.ReplyDTO;
 import org.pgm.japdemo.service.ReplyService;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,6 +35,38 @@ public class ReplyController {
         resultMap.put("rno",rno);
         return resultMap;
     }
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno,
+                                             PageRequestDTO pageRequestDTO){
+
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno, pageRequestDTO);
+
+        return responseDTO;
+    }
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO( @PathVariable("rno") Long rno ){
+        ReplyDTO replyDTO = replyService.findById(rno);
+        return replyDTO;
+    }
+    @DeleteMapping("/{rno}")
+    public Map<String,Long> remove( @PathVariable("rno") Long rno ){
+        replyService.remove(rno);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", rno);
+        return resultMap;
+    }
+
+    @PutMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE )
+    public Map<String,Long> modify( @PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO ){
+        replyDTO.setRno(rno); //번호를 일치시킴
+        replyService.modify(replyDTO);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", rno);
+        return resultMap;
+    }
+
+
+
 
 
 }
